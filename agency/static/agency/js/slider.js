356bef2b88c2c5f44d7fea57fc0bd0cb9787a2bf
Icedevil777 <icedevil777228@mail.ru>
2024-05-16 21:56:55 +0300

    add slider

---
+++ agency/static/agency/js/slider.js
@@ -0,0 +1,30 @@
+// $(document).ready(function(){
+//     $('.slick-slider').slick({
+//       "setting-name": "setting-value",
+//       infinite: true,
+//       slidesToShow: 3,
+//       slidesToScroll: 3
+//     });
+// });
+
+
+$(document).ready(function(){
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav',
+        
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true,
+        variableWidth: true
+    });
+
+});
